310692f6555fdb69f6c04fde70aa6015f9c045f3
mitsuruyamaura <komayou326@gmail.com>
2018-08-26 12:41:05 +0900

    ScriptTest　提出課題のコミットです。
    課題：ScriptTestにて、配列の宣言と出力をするを作成したものです。よろしくお願い致します。

---
+++ Assets/Test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Test : MonoBehaviour {
+
+	void Start(){
+        //  5つの要素の配列を初期化する
+        int[] array = { 50, 100, 35, 88, 42};
+
+        //  配列の要素数だけ処理を繰り返す
+        for (int i = 0; i < array.Length; i++){
+            //  配列の要素を順番に表示する
+            Debug.Log(array[i]);
+        }
+
+        //  for文を使い、配列の各要素の値を逆順に表示する
+        for (int i = 4; array.Length > i;i--){
+            Debug.Log(array[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
